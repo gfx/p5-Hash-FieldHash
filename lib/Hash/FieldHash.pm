9329cb39d5b74c9963aa5dda4e97b8b1265fd5da
gfx <gfuji@cpan.org>
2010-01-06 15:23:18 +0900

    Checking in changes prior to tagging of version 0.09.  Changelog diff is:
    diff --git a/Changes b/Changes
    index a366eb5..3fcad07 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,10 @@
     Revision history for Perl extension Hash::FieldHash
    
    +0.09 Wed Jan  6 15:06:58 2010
    +	- Make warnings on subroutine redefinition (RT #52073)
    +	- Minimum perl version is now 5.8.5
    +	- Modernize Makefile.PL
    +
     0.08 Mon Jun 15 22:45:02 2009
     	- update ppport.h to 3.19

--- lib/Hash/FieldHash.pm
+++ lib/Hash/FieldHash.pm
@@ -3,7 +3,7 @@ package Hash::FieldHash;
 use 5.008_005;
 use strict;
 
-our $VERSION = '0.08';
+our $VERSION = '0.09';
 
 use parent qw(Exporter);
 our @EXPORT_OK   = qw(fieldhash fieldhashes from_hash to_hash);
@@ -29,7 +29,7 @@ Hash::FieldHash - A lightweight field hash implementation
 
 =head1 VERSION
 
-This document describes Hash::FieldHash version 0.08.
+This document describes Hash::FieldHash version 0.09.
 
 =head1 SYNOPSIS
 
