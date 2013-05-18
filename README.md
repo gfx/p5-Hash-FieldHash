3ed76cd1eba153c236b443dc15d638d79c1f5287
Fuji, Goro <g.psy.va@gmail.com>
2013-05-18 16:54:34 -0700

    Checking in changes prior to tagging of version 0.14.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index e1dfb29..2f3d75c 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,9 @@ Revision history for Perl extension Hash::FieldHash
    
     {{$NEXT}}
    
    +0.14 2013-05-18T23:54:21Z
    +    - Fix a build issue on 5.8.x
    +
     0.13 2013-04-13T11:09:06Z
         - Use Module::Build instead of Module::Install with minil(1)
         - No code changes

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Hash::FieldHash - Lightweight field hash for inside-out objects
 
 # VERSION
 
-This document describes Hash::FieldHash version 0.13.
+This document describes Hash::FieldHash version 0.14.
 
 # SYNOPSIS
 
--- lib/Hash/FieldHash.pm
+++ lib/Hash/FieldHash.pm
@@ -3,7 +3,7 @@ package Hash::FieldHash;
 use 5.008_005;
 use strict;
 
-our $VERSION = '0.13';
+our $VERSION = '0.14';
 
 use parent qw(Exporter);
 our @EXPORT_OK   = qw(fieldhash fieldhashes from_hash to_hash);
@@ -29,7 +29,7 @@ Hash::FieldHash - Lightweight field hash for inside-out objects
 
 =head1 VERSION
 
-This document describes Hash::FieldHash version 0.13.
+This document describes Hash::FieldHash version 0.14.
 
 =head1 SYNOPSIS
 
